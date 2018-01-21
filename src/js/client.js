7edf74bf21619a547196b759dded383470c60c02
tanner <tanner.drake@protonmail.com>
2018-01-21 16:34:11 -0500

    grab all necessary dom references

--- src/js/client.js
+++ src/js/client.js
@@ -3,12 +3,54 @@ let Client = {};
 function init() {
     Client.socket = io.connect();
 
+    //#region Grab/Store DOM References
     Client.domRefs = {};
+
+    let newRecordBtn = document.getElementById('new-record-button');
+    if (newRecordBtn != null) {
+        Client.domRefs.newRecordBtn = newRecordBtn;
+    }
+
+    let deleteRecordBtn = document.getElementById('delete-record-button');
+    if (deleteRecordBtn != null) {
+        Client.domRefs.deleteRecordBtn = deleteRecordBtn;
+    }
 
     let tableRef = document.getElementById('table-dom');
     if (tableRef != null) {
         Client.domRefs.table = tableRef;
     }
+
+    let tableList = document.getElementById('table-list');
+    if (tableList != null) {
+        Client.domRefs.tableList = tableList;
+    }
+
+    let newTableBtn = document.getElementById('new-table-button');
+    if (newTableBtn != null) {
+        Client.domRefs.newTableBtn = newTableBtn;
+    }
+
+    let deleteTableBtn = document.getElementById('delete-table-button');
+    if (deleteTableBtn != null) {
+        Client.domRefs.deleteTableBtn = deleteTableBtn;
+    }
+
+    let columnList = document.getElementById('column-list');
+    if (columnList != null) {
+        Client.domRefs.columnList = columnList;
+    }
+
+    let newColumnBtn = document.getElementById('new-column-button');
+    if (newColumnBtn != null) {
+        Client.domRefs.newColumnBtn = newColumnBtn;
+    }
+
+    let deleteColumnBtn = document.getElementById('delete-column-button');
+    if (deleteColumnBtn != null) {
+        Client.domRefs.deleteColumnBtn = deleteColumnBtn;
+    }
+    //#endregion
 }
 
 document.onload = function () {
